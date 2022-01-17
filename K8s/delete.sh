dd962ab4d093e24f60dc90668cf1acbebed08cd9
nohierhassan <nohierassan2@gmail.com>
2022-01-17 08:28:38 +0000

    Resource Managment has been added.

--- K8s/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete deployment.apps/drkiq-deployment
-kubectl delete deployment.apps/nginx-deployment
-kubectl delete deployment.apps/postgres-deployment
-kubectl delete deployment.apps/redis-deployment
-kubectl delete deployment.apps/sidekiq-deployment 
